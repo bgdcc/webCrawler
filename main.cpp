e67dac22b6dae4ffbe0b60f39bed3e0c38464999
Bogdan Culea <bogdculea@gmail.com>
2025-07-15 17:11:50 +0200

    created method to download html in file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
-we
+// #include <bits/stdc++.h>
+#include <unordered_map>
+#include <curl/curl.h>
+#include <stdio.h>
+#include <regex>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+mutex m;
+
+void getPage(const char* url, const char* fileName) {
+    CURL* easyHandle = curl_easy_init();
+    
+    curl_easy_setopt(easyHandle, CURLOPT_URL, url);
+    FILE* file = fopen(fileName, "w");
+
+    curl_easy_setopt(easyHandle, CURLOPT_URL, file);
+    curl_easy_perform(easyHandle);
+    curl_easy_cleanup(easyHandle);
+    fclose(file);
+}
